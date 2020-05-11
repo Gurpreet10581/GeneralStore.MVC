7fc2df9ed77f8a2c1153eb2723421d06fe6f950c
Gurpreet Singh <er.gurpreet.2017@gmail.com>
2020-05-11 18:33:04 -0400

    Add project files.

---
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,144 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Add the application DB Context (link to the database)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            // See below (modifying ApplicationDbContext class)
+            return View(_db.Products.ToList());
+        }
+
+
+
+        //Create 
+
+        //Get:Product
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();//This is all we need, as it will provide a means to enter data.
+        }
+
+
+
+        //Post
+
+
+        //Post:Product
+        //We need to pass in the product that is being recorded in the database.
+        //Remember, ModelState acts like a monitor that checks the status of the data. ModelState will be set to an invalid state, if there is a problem with a key, or if any data is corrupt. 
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //Delete
+
+
+        // GET : Delete
+        // Product/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+
+        // POST : Delete
+        // Product/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Product product = _db.Products.Find(id);
+            _db.Products.Remove(product);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        //Edit
+
+        // GET : Edit
+        // Product/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+
+        // POST : Edit// Product/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(product).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+
+        //Detail
+
+        //This is just a GET request, although you still have to be careful to describe the nullable int (int? notation) in the parameter list for the Details method.
+        // GET : Details
+        // Product/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+        }
+
+    }
+}
